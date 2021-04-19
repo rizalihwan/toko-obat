22a4e058a22864f7a0d139f9f77ed546e37bbec4
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-04-19 21:37:47 +0700

    tambahkan aturan auth

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('drug', 'DrugController');
-Route::resource('supply', 'SupplyController');
-Route::get('/customer/buy', 'BuyDrugController@buy')->name('customer.buy');
-Route::get('/customer/{id}/payment', 'BuyDrugController@payment')->name('customer.payment');
-Route::patch('/customer/{id}/pay', 'BuyDrugController@pay')->name('customer.pay');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('drug', 'DrugController');
+    Route::resource('supply', 'SupplyController');
+    Route::get('/customer/buy', 'BuyDrugController@buy')->name('customer.buy');
+    Route::get('/customer/{id}/payment', 'BuyDrugController@payment')->name('customer.payment');
+    Route::patch('/customer/{id}/pay', 'BuyDrugController@pay')->name('customer.pay');
+});
